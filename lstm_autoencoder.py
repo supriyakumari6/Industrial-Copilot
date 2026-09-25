18770bc1e96cc243057aaee0a0fefa15d9c40c64
supriyakumari6 <supriyakumari98011@gmail.com>
2026-09-25 14:08:49 +0530

    Create lstm_autoencoder.py

---
+++ lstm_autoencoder.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+
+
+class LSTMAutoencoder(nn.Module):
+
+    def __init__(self, input_dim, hidden_dim=64):
+        super().__init__()
+
+        self.encoder = nn.LSTM(
+            input_dim,
+            hidden_dim,
+            batch_first=True
+        )
+
+        self.decoder = nn.LSTM(
+            hidden_dim,
+            input_dim,
+            batch_first=True
+        )
+
+    def forward(self, x):
+        encoded, _ = self.encoder(x)
+        decoded, _ = self.decoder(encoded)
+        return decoded
